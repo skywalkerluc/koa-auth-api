49c5177db13a66564fd441e39740f6911b1ecd97
Ferreira <lucas.h.ferreira@accenture.com>
2019-06-26 16:50:24 -0300

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-dynamic-require */
+import common from './env/common';
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(`./env/${env}`).default;
+
+export default Object.assign({}, common, config);
